0f8b9d85d90d215eda096079272e260a93f4b54d
shenskho <amir.pampay@gmail.com>
2025-08-27 10:21:16 +0330

    license req

--- src/router/routes/home/index.js
+++ src/router/routes/home/index.js
@@ -21,6 +21,8 @@ const EducationField =lazy (()=>import("@views/baseData/variableData/EducationFi
 const Job =lazy (()=>import("@views/baseData/variableData/Job"))
 const SelectionState =lazy (()=>import("@views/baseData/variableData/SelectionState"))
 const AgencyCategory =lazy (()=>import("@views/baseData/variableData/AgencyCategory"))
+const LicenseRequest = lazy(() => import('@views/license/licenseRequest'))
+const RequestLicense = lazy(() => import('@views/license/RequestLicense'))
 
 
 
